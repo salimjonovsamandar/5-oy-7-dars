b775a06d2747b4e340cfef9de15261cdd0890e80
salimjonovsamandar <salimjonovsamandarbek777@gmail.com>
2024-01-11 02:21:50 +0500

    vazifa bajarilfi

--- js/filter.js
+++ js/filter.js
@@ -1,11 +1,15 @@
 import { getDataFromLocalStorage, createCard } from "./function.js";
 
 const wrapper = document.getElementById("product-list");
+const button = document.getElementById("button");
+const color = document.getElementById("color");
+const type = document.getElementById("type");
+const maxPrice = document.getElementById("max-price");
+const minPrice = document.getElementById("min-price");
 
-
+let data = getDataFromLocalStorage();
 
 document.addEventListener("DOMContentLoaded", function () {
-  let data = getDataFromLocalStorage();
   if (data.length) {
     data.forEach((phone) => {
       let card = createCard(phone);
@@ -13,3 +17,58 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+button &&
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    let filter = {};
+
+    if (color.value) {
+      filter.color = color.value;
+    }
+    if (type.value) {
+      filter.type = type.value;
+    }
+    if (minPrice.value) {
+      filter.minPrice = minPrice.value;
+    }
+    if (maxPrice.value) {
+      filter.maxPrice = maxPrice.value;
+    }
+
+    let filteredData = JSON.parse(JSON.stringify(data));
+
+    if (filter.color) {
+      filteredData = filteredData.filter((el) => {
+        return el.color == filter.color;
+      });
+    }
+
+    if (filter.type) {
+      filteredData = filteredData.filter((el) => {
+        return el.type == filter.type;
+      });
+    }
+    if (filter.minPrice) {
+      filteredData = filteredData.filter((el) => {
+        return el.price >= filter.minPrice;
+      });
+    }
+    if (filter.maxPrice) {
+      filteredData = filteredData.filter((el) => {
+        return el.price <= filter.maxPrice;
+      });
+    }
+    if (minPrice.value > maxPrice.value){
+alert("MinPrice MaxPrice dan kotta bo'lishi mumkin emas")
+    } 
+    wrapper.innerHTML = "";
+    if (filteredData.length) {
+      filteredData.forEach((phone) => {
+        let card = createCard(phone);
+        wrapper.innerHTML += card;
+      });
+    } else {
+      wrapper.innerHTML = "Bunday mahsulot mavjud emas!!!";
+    }
+  });
